33a92ea63c6bf4008479bf90f2b366f7b706620b
Nicky Wakim <wakim@ohsu.edu>
2024-10-15 15:56:14 -0700

    Updating 10-16 slides

---
+++ lessons/slides_color_palette.R
@@ -0,0 +1,18 @@
+# Define the custom color palette using HEX codes
+palette_525 <- c("#5BAFF8",
+                 "#EF85B3",
+                 "#E75B5C",
+                 "#8043F3", 
+                 "#367B79",
+                 "#3070BF",
+                 "#BF396F",                 
+                 "#C83532",
+                 "#9F71F7",
+                 "#459B99",
+                 "#FDF3F2", 
+                 "#6C757D"
+)
+
+# # To visualize the palette in R, you can use:
+# library(scales)
+# show_col(my_palette)
